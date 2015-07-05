b93b9c3ef83231d7bad7cca6857c262d8707fe89
AlexDigital <creepplay2015@gmail.com>
2015-07-05 19:34:30 +0200

    [+] Countdown class

---
+++ src/de/janhektor/community/utils/Countdown.java
@@ -0,0 +1,67 @@
+package de.janhektor.community.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitTask;
+
+import de.janhektor.community.Main;
+
+/**
+ * Useful countdown class to make countdowns easily.
+ * @author Alex
+ */
+public class Countdown {
+	
+	private int startTime;
+	private ICountdownListener countdownListener;
+	
+	private int countdown;
+	private BukkitTask task;
+	
+	/**
+	 * Useful countdown class to make countdowns easily.
+	 * @param startTime Start time of the countdown
+	 * @param countdownListener The countdown listener
+	 */
+	public Countdown(int startTime, ICountdownListener countdownListener) {
+		this.startTime = startTime;
+		this.countdownListener = countdownListener;
+	}
+	
+	private void sendCountdown() {
+		Bukkit.getOnlinePlayers().forEach((player) -> player.sendMessage(Main.getInstance().getString("Countdown", countdown)));
+	}
+	
+	public void start() {
+		this.countdown = this.startTime;
+		task = Bukkit.getServer().getScheduler().runTaskTimer(Main.getInstance(), new Runnable() {
+			
+			@Override
+			public void run() {
+				if (Countdown.this.countdown % 30 == 0 || Countdown.this.countdown <= 10) {
+					Countdown.this.sendCountdown();
+					Countdown.this.countdownListener.onCountdown(countdown);
+				} else if (countdown == 0) {
+					Countdown.this.countdownListener.onCountdown(0);
+				} else if (countdown < 0) {
+					Countdown.this.countdownListener.onEnd();
+					Countdown.this.stop();
+				}
+			}
+			
+		}, 0L, 20L);
+	}
+	
+	public void stop() {
+		if (this.task != null) {
+			this.task.cancel();
+			this.task = null;
+		}
+	}
+	
+	public static interface ICountdownListener {
+		
+		public void onCountdown(int time);
+		public void onEnd();
+		
+	}
+}
