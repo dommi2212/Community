19468fd481d937d18a85753309a18e61e258506a
Aquaatic <aquaticvfx@gmail.com>
2015-07-05 12:51:11 +0200

    [+] Added LocationManager

---
+++ src/de/janhektor/community/config/LocationManager.java
@@ -0,0 +1,58 @@
+package de.janhektor.community.config;
+
+import de.janhektor.community.Main;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public class LocationManager {
+
+    private YamlConfiguration configuration;
+    private File file;
+
+    public LocationManager() {
+        createFile();
+    }
+
+    public Location getLocation(String key) {
+        Object value = configuration.get(key);
+        if(value instanceof Map) {
+            return Location.deserialize((Map<String, Object>) value);
+        }
+        return null;
+    }
+
+    public void setLocation(Location loc, String key) {
+        this.configuration.set(key, loc.serialize());
+    }
+
+    private void createFile() {
+        Main.inst().getDataFolder().mkdirs();
+        if(this.file == null)
+            this.file = new File(Main.inst().getDataFolder(), "locations.yml");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(this.configuration == null)
+            configuration = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public void save() {
+        createFile();
+        try {
+            configuration.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void reload() {
+        createFile();
+        this.configuration = YamlConfiguration.loadConfiguration(file);
+    }
+}
